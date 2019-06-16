caec0ef8785c98ab739ad340c0f98baf5583cc1f
MalihaKabir <malihakabir01@gmail.com>
2019-06-17 00:52:41 +0600

    secure api

--- controllers/imgEntry.js
+++ controllers/imgEntry.js
@@ -2,8 +2,7 @@ const Clarifai = require('clarifai');
 
 // You MUST get your own API from clarifai. See README.md file for details.
 const app = new Clarifai.App({
-	apiKey: '036f526ad4d4471d87379a83017d3cbf'
-	// apiKey: 'Your_Own_API_KEY'
+	apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
